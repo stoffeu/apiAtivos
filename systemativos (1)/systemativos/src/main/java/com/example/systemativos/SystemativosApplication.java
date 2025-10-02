3b94fa413d7e2ef7eb32919533cbf8404a9c9f3d
stoffeu <kaikestoffell@gmail.com>
2025-10-02 19:50:08 -0300

    Add files via upload

---
+++ systemativos (1)/systemativos/src/main/java/com/example/systemativos/SystemativosApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemativos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemativosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemativosApplication.class, args);
+	}
+
+}
---
+++ systemativos (1)/systemativos/src/test/java/com/example/systemativos/SystemativosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemativos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemativosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
